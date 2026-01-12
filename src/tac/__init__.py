017c2d5e3e6ba8418c496d43396790c4aa4f40a7
Jeremie Gince <jeremie.gince@usherbrooke.ca>
2026-01-12 16:40:20 -0500

    Add tests for package metadata and dynamically set `__version__` in `__init__.py`

--- src/tac/__init__.py
+++ src/tac/__init__.py
@@ -1,9 +1,12 @@
+import importlib_metadata
+
 __author__ = "Jérémie Gince"
 __email__ = "gincejeremie@gmail.com"
 __copyright__ = "Copyright 2023, Jérémie Gince"
 __license__ = "Apache 2.0"
 __url__ = "https://github.com/JeremieGince/TPAutoCorrect"
-__version__ = "0.0.1-beta0"
+__package__ = "tac"
+__version__ = importlib_metadata.version(__package__)
 
 import warnings
 
