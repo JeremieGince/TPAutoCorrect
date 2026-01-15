293cdc005eb09233e7d2674f4f9a38e408991ac6
Julien Drapeau <juliendrapeau707@gmail.com>
2026-01-15 17:26:50 -0500

    Install TAC in tests workflow

--- src/tac/__init__.py
+++ src/tac/__init__.py
@@ -6,7 +6,11 @@ __copyright__ = "Copyright 2023, Jérémie Gince"
 __license__ = "Apache 2.0"
 __url__ = "https://github.com/JeremieGince/TPAutoCorrect"
 __package__ = "tac"
-__version__ = importlib_metadata.version(__package__)
+
+try:
+    __version__ = importlib_metadata.version(__package__)
+except importlib_metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
 
 import warnings
 
